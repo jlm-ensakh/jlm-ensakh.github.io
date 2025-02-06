d7d5f0fde0c0c731bc3c786a933decc532aa94d7
itsachrafmansari <contact@achrafmansari.com>
2025-02-06 23:21:13 +0100

    Modified the github actions deployment config file according to Vite recommended config

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: 'jlm-ensakh.github.io',
   plugins: [
     react(),
     tailwindcss(),
