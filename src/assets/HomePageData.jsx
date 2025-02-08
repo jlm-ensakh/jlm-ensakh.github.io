6a91d2afff72a56042544ffc64d57cca0515c00f
itsachrafmansari <contact@achrafmansari.com>
2025-02-09 00:32:41 +0100

    Added the STAFF_MEMBERS list to the HomePageData file and used it to create the staff members section the home page

--- src/assets/HomePageData.jsx
+++ src/assets/HomePageData.jsx
@@ -1,5 +1,17 @@
 import { faInstagramSquare, faLinkedin } from "@fortawesome/free-brands-svg-icons"
 
+export const STAFF_MEMBERS = [
+    { firstName: "First Name", lastName: "Last Name", role: "Role as a staff member of the club", portraitImage: "https://alkhawarizmiclub.github.io/assets/images/member-image-1.png", socialMedia: [{ url: "#", icon: faLinkedin }, { url: "#", icon: faInstagramSquare }] },
+    { firstName: "Achraf", lastName: "Mansari", role: "Vice-Team Leader", portraitImage: "https://media.licdn.com/dms/image/v2/D4E03AQEvgYA2w2uxtQ/profile-displayphoto-shrink_400_400/B4EZSOnxjZHAAs-/0/1737559590789?e=1744243200&v=beta&t=XrIYKyAY0mMRnUCnqfR1PkhfmeCP7OevYIZqW4JhW90", socialMedia: [{ url: "#", icon: faLinkedin }, { url: "#", icon: faInstagramSquare }] },
+    { firstName: "First Name", lastName: "Last Name", role: "Role as a staff member of the club", portraitImage: "https://alkhawarizmiclub.github.io/assets/images/member-image-1.png", socialMedia: [{ url: "#", icon: faLinkedin }, { url: "#", icon: faInstagramSquare }] },
+    { firstName: "First Name", lastName: "Last Name", role: "Role as a staff member of the club", portraitImage: "https://alkhawarizmiclub.github.io/assets/images/member-image-1.png", socialMedia: [{ url: "#", icon: faLinkedin }, { url: "#", icon: faInstagramSquare }] },
+    { firstName: "First Name", lastName: "Last Name", role: "Role as a staff member of the club", portraitImage: "https://alkhawarizmiclub.github.io/assets/images/member-image-1.png", socialMedia: [{ url: "#", icon: faLinkedin }, { url: "#", icon: faInstagramSquare }] },
+    { firstName: "First Name", lastName: "Last Name", role: "Role as a staff member of the club", portraitImage: "https://alkhawarizmiclub.github.io/assets/images/member-image-1.png", socialMedia: [{ url: "#", icon: faLinkedin }, { url: "#", icon: faInstagramSquare }] },
+    { firstName: "First Name", lastName: "Last Name", role: "Role as a staff member of the club", portraitImage: "https://alkhawarizmiclub.github.io/assets/images/member-image-1.png", socialMedia: [{ url: "#", icon: faLinkedin }, { url: "#", icon: faInstagramSquare }] },
+    { firstName: "First Name", lastName: "Last Name", role: "Role as a staff member of the club", portraitImage: "https://alkhawarizmiclub.github.io/assets/images/member-image-1.png", socialMedia: [{ url: "#", icon: faLinkedin }, { url: "#", icon: faInstagramSquare }] },
+    { firstName: "First Name", lastName: "Last Name", role: "Role as a staff member of the club", portraitImage: "https://alkhawarizmiclub.github.io/assets/images/member-image-1.png", socialMedia: [{ url: "#", icon: faLinkedin }, { url: "#", icon: faInstagramSquare }] },
+]
+
 export const SOCIAL_MEDIA_PAGES = [
     { url: "#", icon: faLinkedin },
     { url: "#", icon: faInstagramSquare },
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,8 @@
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFacebookSquare, faInstagramSquare, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import { STAFF_MEMBERS } from "../assets/HomePageData";
 import SectionContainer from "../components/SectionContainer";
 
 const HomePage = () => {
@@ -28,6 +31,41 @@ const HomePage = () => {
                 </div>
             </SectionContainer>
             {/* Why Join Us section | ends */}
+
+            {/* Staff Members section | starts */}
+            <SectionContainer className="bg-slate-100">
+                <h1 className="mb-10 text-center text-4xl text-slate-700 font-bold">Staff Members</h1>
+
+                <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-5">
+                    {STAFF_MEMBERS.map(({ portraitImage, firstName, lastName, role }, idx) => (
+
+                        <div key={idx} className="flex items-center rounded-lg bg-white shadow-lg">
+
+                            {/* Portrait image */}
+                            <div className="aspect-[3/4] w-full max-w-38 overflow-hidden rounded-l-lg bg-gray-400">
+                                <img src={portraitImage} alt={`${firstName} ${lastName}'s portrait image`} className="object-cover h-full w-full" />
+                            </div>
+
+                            {/* Personal info */}
+                            <div className="p-5 w-auto flex flex-col gap-2.5">
+                                <h1 className="flex flex-col text-lg text-slate-700 uppercase font-bold">
+                                    <span>{firstName}</span>
+                                    <span>{lastName}</span>
+                                </h1>
+                                <p>{role}</p>
+                                <div className="flex gap-2">
+                                    <a href="#"><FontAwesomeIcon icon={faLinkedin} className="text-orange-400 hover:text-orange-500 transition-color duration-200" size="xl" /></a>
+                                    <a href="#"><FontAwesomeIcon icon={faFacebookSquare} className="text-orange-400 hover:text-orange-500 transition-color duration-200" size="xl" /></a>
+                                    <a href="#"><FontAwesomeIcon icon={faInstagramSquare} className="text-orange-400 hover:text-orange-500 transition-color duration-200" size="xl" /></a>
+                                </div>
+                            </div>
+
+                        </div>
+
+                    ))}
+                </div>
+            </SectionContainer>
+            {/* Staff Members section | ends */}
 
             <Footer />
         </>
